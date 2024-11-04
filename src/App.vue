d57b614f2ecda4bbc10241f7ccf827bdd19641d9
blue-Jundy <dimasuay.jm08@gmail.com>
2024-11-04 19:40:31 +0800

    Update bg cover

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,6 @@ import Header from "./components/Layouts/Header.vue";
 
 <style scoped>
 main {
-  @apply w-full h-screen bg-[radial-gradient(circle_at_-70%_-40%,rgb(255,198,134)0%,rgb(255,210,158)35%,rgb(238,226,211)69%,rgb(255,248,240)80%)] dark:bg-[radial-gradient(circle_at_-70%_-40%,rgb(59,59,74)0%,rgb(40,40,74)35%,rgb(33,33,44)69%,rgb(30,30,36)80%)] bg-no-repeat bg-cover;
+  @apply w-full h-screen bg-[radial-gradient(circle_at_-70%_-40%,rgb(255,198,134)_0%,rgb(255,210,158)_35%,rgb(238,226,211)_69%,rgb(255,248,240)_80%)] dark:bg-[radial-gradient(circle_at_-70%_-40%,rgb(59,59,74)_0%,rgb(40,40,74)_35%,rgb(33,33,44)_69%,rgb(30,30,36)_80%)] bg-no-repeat bg-cover;
 }
 </style>
